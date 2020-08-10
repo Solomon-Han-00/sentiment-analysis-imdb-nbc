f6a75e0d7dcf08431d6a5559cd15bba2ac572501
Solomon-Han-00 <upgrace00@gmail.com>
2020-08-10 15:20:03 +0900

    Updates

---
+++ precache-manifest.b568a71b78a7a0e0774868c1b88db5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26301eb5cc4ec971da1977e1ccf01fa",
+    "url": "/sentiment-analysis-imdb-nbc/index.html"
+  },
+  {
+    "revision": "9c9851e65a619cf9475d",
+    "url": "/sentiment-analysis-imdb-nbc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a758c448704a2bebe9f9",
+    "url": "/sentiment-analysis-imdb-nbc/static/js/2.e729f44f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sentiment-analysis-imdb-nbc/static/js/2.e729f44f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9851e65a619cf9475d",
+    "url": "/sentiment-analysis-imdb-nbc/static/js/main.a3a7c617.chunk.js"
+  },
+  {
+    "revision": "2505bb5a20bc46904325",
+    "url": "/sentiment-analysis-imdb-nbc/static/js/runtime-main.d1ae2f54.js"
+  }
+]);
